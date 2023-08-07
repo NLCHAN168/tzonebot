28a299a812a9462c5ba783748d4cf2a2a620ed96
Nick <nlchan168@yahoo.com>
2023-08-07 11:58:55 -0400

    fixed import statement in tzonebot.js

--- tzonebot.js
+++ tzonebot.js
@@ -1,11 +1,11 @@
 import Tesseract from "tesseract.js";
-import checkString from zonechecker.js;
+import checkString from "./zonechecker.js";
 
 Tesseract.recognize(
   "https://thegodofpumpkin.com/terrorzones/terrorzone.png",
   "eng",
   { logger: (m) => console.log(m) }
 ).then(({ data: { text } }) => {
-    //checkString(m)
+  checkString(text);
   console.log(text);
 });
