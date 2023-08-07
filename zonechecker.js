0adbaaf8b532fc9054e0847e6c2887f6e56215f5
Nick <nlchan168@yahoo.com>
2023-08-07 05:28:23 -0400

    simplify string checker

--- zonechecker.js
+++ zonechecker.js
@@ -1,13 +1,22 @@
 let chaos, baal, tombs;
 
-chaos = "The Chaos Sanctuary";
-baal =
-  "Worldstone Keep 1 / Worldstone Keep 2 / liorldstone Keep 3 / Throne of Destruction / Worldstone Keep";
-tombs =
-  "Tal Rashas Tomb 1 / Tal Rashas Tomb 2 / Tal Rashas Tomb 3 / Tal Rashas Tomb ¢ / Tal Rashas Tomb § / Tal Rashas Tomb 6 / Tal Rashas Tomb 7 / Tal Rashas Chamber";
+chaos = "Chaos";
+baal = "Worldstone";
+tombs = "Tal";
 
 export default function checkString(string) {
-  if (string == chaos || string == baal || string == tombs) {
-    // return string + ping role
-  }
+  let newString = string;
+  let words = string.split(" ");
+  words.forEach((element) => {
+    if (compare(element)) {
+      // newString = string + discordping
+    }
+    return newString;
+  });
+}
+
+function compare(item) {
+  if (item == chaos || item == baal || item == tombs) {
+    return true;
+  } else return false;
 }
