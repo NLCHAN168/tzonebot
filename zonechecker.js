9639f6df9182229e86fa7afe80ed1aff87b75eae
Nick <nlchan168@yahoo.com>
2023-08-17 05:58:45 -0400

    changed for loop to for-of

--- zonechecker.js
+++ zonechecker.js
@@ -2,8 +2,8 @@ let zones = ["chaos", "worldstone", "tal"];
 
 export default function checkString(string) {
   let words = string.split(" ");
-  for (let i = 0; i < words.length; i++) {
-    if (zones.includes(words[i].toLowerCase())) {
+  for (const element of words) {
+    if (zones.includes(element.toLowerCase())) {
       return true;
     }
   }
